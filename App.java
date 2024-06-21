5d894da14a885c6a167602cc8da7b73ab1134581
Michal Budzowski <michal.budzowski@sonarsource.com>
2024-06-21 11:40:51 +0200

    add App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(getLength("Hello World"));
+  }
+  
+  
+  public int getLength(String st){
+  	return st.length();
+  }
+  
+}
+
